09bb1d7420f5dc4c598986155d593969c8713a17
THEjobish <55290815+THEjobish@users.noreply.github.com>
2019-10-05 14:12:14 -0700

    hh

--- j2.js
+++ j2.js
@@ -4,19 +4,21 @@ function validateForm() {
     alert("Please input a name.");
     return false;
   }
-   var y = document.forms["myForm"]["age"].value;
+   
+  var y = document.forms["myForm"]["age"].value;
    if (y < 18 || y > 110) {
 	   alert("Age must be between 18 and 110.");
 	   return false;
    }
    var z = document.forms["myForm"]["radio"].value;
    if (z < 1) {
-	   alert("Please select a school".);
+	   alert("Please select a school.");
 	   return false;
    }
-  
-  
-  
+  else {
+	  //don't need to return true if condition is assumed true
+	  alert("Success!");
+  }
   
   
   
